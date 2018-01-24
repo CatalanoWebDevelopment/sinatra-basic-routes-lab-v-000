460bba8300eaf8335d70a63a3ca8eaab26815b94
Parker Catalano <catalano.work@gmail.com>
2018-01-24 18:53:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/name" do
+    "My name is Parker Catalano"
+  end
+
+  get "/hometown" do
+    "My hometown is Carmel"
+  end
 end
